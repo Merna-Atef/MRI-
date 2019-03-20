807ae9f422ca8dccb5828c5e885d88eab9bf1da4
Joe <youssefgamal84@yahoo.com>
2019-03-20 17:40:12 +0200

    added playground

---
+++ playground.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image
+from phantom import phantom
+
+img = phantom(30)
+f_img = np.fft.fft2(img)
+
+reconstructed = np.fft.ifft2(f_img)
+
+reconstructed2 = np.zeros(np.shape(img), dtype=np.complex_)
+
+for i in range(0, np.shape(img)[0]):
+    reconstructed2[:, i] = np.fft.fft(f_img[:, i])
+for i in range(0, np.shape(img)[0]):
+    reconstructed2[i, :] = np.fft.fft(f_img[i, :])
+
+reconstructed = np.abs(reconstructed)
+reconstructed2 = np.abs(reconstructed2)
+
+img1 = Image.fromarray(reconstructed, 'L')
+img2 = Image.fromarray(reconstructed2, 'L')
+
+img1.show()
+img2.show()
+# from rotation import rotateX, rotateZ
+#
+# matrix = np.zeros((512, 512, 3), dtype=np.complex_)
+# x = np.complex(2,3)
+# print(x)
+# sigmaZ = np.sum(matrix[:, :, 2])
+# print(sigmaZ)
